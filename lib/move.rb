2d89930240d09d5a19388db1a71149568f908900
Thomas John Helin <thomashelin@yahoo.com>
2020-03-17 17:12:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,4 +12,9 @@ def input_to_index(number)
 end
 
 def move(board, number, players_character="X")
+  board = [" ", " ", " "]
+  def update_array_at_with(array, index, value)
+    array[index] = value
+  end
+   update_array_at_with(board, 0, "X")
 end
